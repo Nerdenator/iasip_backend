0ea4ed1e4eace591bb17ec650d30dec9feb730bd
Aaron McRuer <a.mcruer@live.com>
2017-04-17 12:11:19 -0500

    fixed syntax issue in urls.py

--- iasip_api/urls.py
+++ iasip_api/urls.py
@@ -2,25 +2,29 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from iasip_api import views
 
+
 # API endpoints
+
 urlpatterns = format_suffix_patterns([
     url(r'^$', views.api_root),
     url(r'^characters/$',
         views.CharacterList.as_view(),
         name='character-list'),
     url(r'^characters/(?P<pk>[0-9]+)/$',
-        view.CharacterDetail.as_view(),
-        name='character-detail'),
+        views.CharacterDetail.as_view(),
+        name='snippet-detail'),
     url(r'^characters/(?P<pk>[0-9]+)/highlight/$',
         views.CharacterHighlight.as_view(),
-        name='character-highlight'),
+        name='characters-highlight'),
     url(r'^users/$',
         views.UserList.as_view(),
         name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$,
+    url(r'^users/(?P<pk>[0-9]+)/$',
         views.UserDetail.as_view(),
         name='user-detail')
 ])
+
+
 
 urlpatterns += [
     url(r'^api-auth/', include('rest_framework.urls',
