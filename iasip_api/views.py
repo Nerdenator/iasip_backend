d9d9c01a07be01a1bcca8ca71a92eb5a96ba1da6
Aaron McRuer <a.mcruer@live.com>
2017-04-17 09:10:14 -0500

    Converted over to class based views that make use of generic API views.

--- iasip_api/views.py
+++ iasip_api/views.py
@@ -1,54 +1,16 @@
-from rest_framework import status
-from rest_framework.response import Response
 from iasip_api.models import Character
 from iasip_api.serializers import CharacterSerializer
-from django.http import Http404
-from rest_framework.views import APIView
+from rest_framework import generics
 
-
-class CharacterList(APIView):
+class CharacterList(generics.ListCreateAPIView):
     """
     List all characters, or create a new one
     """
-    def get(self, request, format=None):
-        characters = Character.objects.all()
-        serializer = CharacterSerializer(characters, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = CharacterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
 
 
-class CharacterDetail(APIView):
-    """
-    Retrieve, update, or delete a character instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Character.objects.get(pk=pk)
-        except Character.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        character = self.get_object(pk)
-        serializer = CharacterSerializer(character)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        character = self.get_object(pk)
-        serializer = CharacterSerializer(character, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        character = self.get_object(pk)
-        character.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+class CharacterDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Character.objects.all()
+    serializer_class = CharacterSerializer
 
