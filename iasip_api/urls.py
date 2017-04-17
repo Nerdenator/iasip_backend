ed9a4810bc7d71cf763a8c95d630d9e5d4337427
Aaron McRuer <a.mcruer@live.com>
2017-04-17 08:49:06 -0500

    Shifted over to class-based views.

--- iasip_api/urls.py
+++ iasip_api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from iasip_api import views
 
 urlpatterns = [
-    url(r'^characters/$', views.character_list),
-    url(r'^characters/(?P<pk>[0-9]+)/$', views.character_detail),
+    url(r'^characters/$', views.CharacterList.as_view()),
+    url(r'^characters/(?P<pk>[0-9]+)/$', views.CharacterDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- iasip_api/views.py
+++ iasip_api/views.py
@@ -1,49 +1,54 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from iasip_api.models import Character
 from iasip_api.serializers import CharacterSerializer
+from django.http import Http404
+from rest_framework.views import APIView
 
 
-@api_view(['GET', 'POST'])
-def character_list(request, format=None):
+class CharacterList(APIView):
     """
-    List all characters, or create a new one.
+    List all characters, or create a new one
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         characters = Character.objects.all()
         serializer = CharacterSerializer(characters, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = CharacterSerializer(data=request.data)
-        if serialier.is_valid():
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def character_detail(request, pk, format=None):
-    """
-    Retrieve, update, or delete a character.
-    """
-    try:
-        character = Character.objects.get(pk=pk)
-    except Character.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+class CharacterDetail(APIView):
+    """
+    Retrieve, update, or delete a character instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Character.objects.get(pk=pk)
+        except Character.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        character = self.get_object(pk)
         serializer = CharacterSerializer(character)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        character = self.get_object(pk)
         serializer = CharacterSerializer(character, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        character = self.get_object(pk)
         character.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
