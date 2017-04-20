c33a9f0ad96647f5bf1ebe460adc52a97d996faa
Aaron McRuer <a.mcruer@live.com>
2017-04-20 11:39:30 -0500

    removed debug

--- iasip_backend/settings.py
+++ iasip_backend/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 if DEBUG:
     SECRET_KEY = 'mysecretkey'
