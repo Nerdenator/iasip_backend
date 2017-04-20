e1a04de17c320dcc4a00a3953d37d2d663baa2da
Aaron McRuer <a.mcruer@live.com>
2017-04-20 11:28:47 -0500

    procfile and settings modified

--- iasip_backend/settings.py
+++ iasip_backend/settings.py
@@ -24,6 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+if DEBUG:
+    SECRET_KEY = 'mysecretkey'
+else:
+    SECRET_KEY = os.environ['SECRET_KEY']
+
 ALLOWED_HOSTS = []
 
 
