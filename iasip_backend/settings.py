a3c5e690d24dd96f16c2682debd2607c2655e812
Aaron McRuer <a.mcruer@live.com>
2017-04-20 20:41:07 -0500

    trying to fix css per heroku instructions

--- iasip_backend/settings.py
+++ iasip_backend/settings.py
@@ -126,14 +126,22 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
 
 #CORS stuff
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_METHODS = (
     'GET',
+)
+
+# to serve static assets
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static')
 )
