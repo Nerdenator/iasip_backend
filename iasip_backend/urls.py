c89a004e8018a1735a29471ab1f873f2de9c3ff1
Aaron McRuer <a.mcruer@live.com>
2017-05-02 17:09:34 -0500

    added coreapi and coreapi schema url endpoint.

--- iasip_backend/urls.py
+++ iasip_backend/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='IASIP Backend API')
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('iasip_api.urls')),
+    url(r'^schema/$', schema_view),
 ]
